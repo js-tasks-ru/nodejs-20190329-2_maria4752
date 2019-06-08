c30cdff52b0aff472693e417b0c457a33fc77ee0
MaSirius <sirius_4752@outlook.com>
2019-06-08 19:22:09 +0400

    add solution 6-module 2-task

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -1,5 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const User = require('./models/User');
+const mongoose = require('mongoose');
 
 const app = new Koa();
 
@@ -14,8 +16,17 @@ app.use(async (ctx, next) => {
       ctx.status = err.status;
       ctx.body = {error: err.message};
     } else {
-      ctx.status = 500;
-      ctx.body = {error: 'Internal server error'};
+      if (err.name === 'ValidationError') {
+        ctx.status = 400;
+        const errorMessages = {errors: {}};
+        Object.keys(err.errors).forEach((error) => {
+          errorMessages.errors[error] = err.errors[error].message;
+        });
+        ctx.body = errorMessages;
+      } else {
+        ctx.status = 500;
+        ctx.body = {error: 'Internal server error'};
+      }
     }
   }
 });
@@ -23,23 +34,71 @@ app.use(async (ctx, next) => {
 const router = new Router();
 
 router.get('/users', async (ctx) => {
-
+  ctx.status = 200;
+  ctx.body = await User.find({}).exec();
 });
 
 router.get('/users/:id', async (ctx) => {
+  const id = ctx.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
 
+  const userById = await User.findById(id)
+
+  if (!userById) {
+    ctx.status = 404;
+    return;
+  }
+
+  ctx.status = 200;
+  ctx.body = userById;
 });
 
 router.patch('/users/:id', async (ctx) => {
+  const id = ctx.params.id;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+  const user = await User.findOne({_id: ctx.params.id}).exec();
+  if (user) {
+    if (ctx.request.body.email) {
+      user.email = ctx.request.body.email;
+    }
+    if (ctx.request.body.displayName) {
+      user.displayName = ctx.request.body.displayName;
+    }
+    await user.validate();
+    await user.save();
+
+    ctx.body = await User.findById(ctx.params.id).exec();
+  } else {
+    ctx.status = 404;
+    return;
+  }
 });
 
 router.post('/users', async (ctx) => {
-
+  ctx.body = await User.create({
+    email: ctx.request.body.email, displayName: ctx.request.body.displayName,
+  });
 });
 
 router.delete('/users/:id', async (ctx) => {
+  const id = ctx.params.id;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+  const deletedUser = await User.findByIdAndRemove(id);
+
+  if (deletedUser) {
+    ctx.body = deletedUser;
+  }
 });
 
 app.use(router.routes());
